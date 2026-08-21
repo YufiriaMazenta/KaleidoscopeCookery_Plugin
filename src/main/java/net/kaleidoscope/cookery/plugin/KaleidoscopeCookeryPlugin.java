1cc3537a1427fc982e1faa147b9f1ef72365ff9c
Yufiria <xiaoyi357159@gmail.com>
2026-08-21 19:30:43 +0800

    同步上游1.1.7版本更改

--- src/main/java/net/kaleidoscope/cookery/plugin/KaleidoscopeCookeryPlugin.java
+++ src/main/java/net/kaleidoscope/cookery/plugin/KaleidoscopeCookeryPlugin.java
@@ -26,7 +26,6 @@ import net.kaleidoscope.cookery.api.BlockTags;
 import net.kaleidoscope.cookery.api.ItemTags;
 import net.kaleidoscope.cookery.api.MillstoneAnimals;
 import net.kaleidoscope.cookery.command.RecipeCommand;
-import net.kaleidoscope.cookery.item.listener.DropPatchListener;
 import net.kaleidoscope.cookery.recipe.DishCarriers;
 import net.kaleidoscope.cookery.recipe.FoodGroups;
 import net.kaleidoscope.cookery.recipe.FoodRecipeManager;
@@ -94,7 +93,6 @@ public final class KaleidoscopeCookeryPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new DisplayTrackingListener(), this);
         getServer().getPluginManager().registerEvents(new LunchBagListener(), this);
         getServer().getPluginManager().registerEvents(new CraftEngineRegistryCheckListener(this), this);
-        getServer().getPluginManager().registerEvents(new DropPatchListener(), this);
         if (FoliaUtil.isFolia()) {
             TrashCanRespawnListener.registerFoliaPackets(this);
         } else {
